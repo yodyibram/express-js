0a4654b4b22afc0f3c7130b77e878a04d1f322fc
yodyibram <yody.theblues@gmail.com>
2024-02-08 17:10:37 +0700

    first commit

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const router = require('./routes');
-const log = require('./middlewares/logger');
+const productRouter = require('./app/product/routes');
+const productRouterV2 = require('./app/product_v2/routes');
+const logger = require('morgan');
 
-app.use(log);
+
+app.use(logger('dev'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use('public', express.static(path.join(__dirname, 'uploads')));
-app.use(router);
+app.use('/api/v1', productRouter);
+app.use('/api/v2', productRouterV2);
 app.use((req, res, next) => {
     res.status(404);
     res.send({
